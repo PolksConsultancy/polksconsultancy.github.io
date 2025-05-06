75c56ed0dd91a5df83143a83d617de9b81ad7b68
PolksConsultancy <yogeshwt@polksconsultancy.com>
2025-05-06 12:49:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -890,7 +890,7 @@ var APP = {
                     fieldFlowElement += `<div class="field-row">
                                             <div class="field-label">
                                                 <div>${field.replaceAll('_', ' ')}</div>
-                                                <div class="recordFieldEditOuter" data-record-id="${APP.selectedRecord.id}" data-field-api="${field}" onclick='APP.editPopupDiv(event, "${contactId.trim()}", "${recordModule+"s"}", "${APP.selectedRecord.id}", "${field}");'>
+                                                <div class="recordFieldEditOuter" data-record-id="${APP.selectedRecord.id}" data-field-api="${field}" onclick='APP.editPopupDiv(event, "${contactId.trim()}", "${recordModule}", "${APP.selectedRecord.id}", "${field}");'>
                                                     <div class="recordFieldEdit">
                                                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="m20.41 4.94-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"></path></svg>
                                                         <span class="recordFieldEditText">edit</span>
@@ -3019,7 +3019,7 @@ var APP = {
     editPopupDiv: async function(e, contactId, module, recordId, fieldId) {
         if(!recordId) return;
         if(!APP.contacts[contactId][module]) return;
-        let moduleFieldsList = await APP.moduleFieldsList(module);
+        let moduleFieldsList = await APP.moduleFieldsList(module+"s");
         let inputHtml = "";
         if(["email", "phone", "string", "text", "number"].includes(moduleFieldsList[fieldId].data_type) || ["Email", "First_Name", "Last_Name", "Full_Name", "Phone", "Mobile", "Secondary_Email", "Asst_Phone", "Home_Phone", "Other_Phone"].includes){
             inputHtml = `<input type="text" class="setDataPopupInputText recordEditFormInput" required data-field="${fieldId}" value="${APP.contacts[contactId][module][fieldId]}">`;
@@ -3065,7 +3065,7 @@ var APP = {
                 }
             ]
         };
-        await APP.updateRecord(module, data);
+        await APP.updateRecord(module+"s", data);
         APP.contacts[contactId][module][fieldId] = fieldValue;
         $("#setDataPopupDiv").remove();
     },
